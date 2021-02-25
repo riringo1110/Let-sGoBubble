6d0b7a0abee3f7079aed2d681f473c534772f7e9
Ririko Nagaishi <riry.cucumber@gmail.com>
2021-02-25 23:54:51 +0900

    Initial Commit

---
+++ Let'sGoBubble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Let'sGoBubble
+//
+//  Created by Ririko Nagaishi on 2021/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
